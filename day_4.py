74c5ade4a3e78c294860c7a1c91f4ad6f14c4da2
DreamyChunk <107152151+DreamyChunk@users.noreply.github.com>
2022-12-05 15:08:42 +0100

    updated day 3

--- day_4.py
+++ day_4.py
@@ -2,14 +2,14 @@
 # Write a program that accepts a sentence and calculate
 # the number of upper case letters and lower case letters.
 
-# sentence = input("Enter a sentence:")
-# upper, lower = 0, 0
-# for e in sentence:
-#     if e.isupper():
-#         upper += 1
-#     elif e.islower():
-#         lower += 1
-# print(f"UPPER:{upper}\nLOWER:{lower}")
+sentence = input("Enter a sentence:")
+upper, lower = 0, 0
+for e in sentence:
+    if e.isupper():
+        upper += 1
+    elif e.islower():
+        lower += 1
+print(f"UPPER:{upper}\nLOWER:{lower}")
 
 # Question 15
 # Write a program that computes the value of a+aa+aaa+aaaa
