a7a502a36235cbf2494d7ecf701d8af31a4e4a61
DreamyChunk <107152151+DreamyChunk@users.noreply.github.com>
2022-12-07 00:13:19 +0100

    added day 8 of challenges

---
+++ day_8.py
@@ -0,0 +1,34 @@
+# Question 22
+# Write a program to compute the frequency of the words from the input.
+# The output should output after sorting the key alphanumerically.
+# Suppose the following input is supplied to the program:
+# New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
+
+
+# sentence = input().split()
+# lst = []
+# for w in sentence:
+#     x = (f"{w}:{sentence.count(w)}")
+#     lst.append(x)
+# sorted = sorted(lst)
+# for e in sorted:
+#     print(e)
+
+
+# Question 23
+# Write a method which can calculate square value of number
+
+# class MyClass:
+#     def squared(self, num):
+#         print(num**2)
+
+
+# my_math = MyClass()
+# my_math.squared(7)
+
+# Question 24
+# Python has many built-in functions, and if you do not know how to use it,
+# you can read document online or find some books.
+# But Python has a built-in document function for every built-in functions.
+# Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+# And add document for your own function
