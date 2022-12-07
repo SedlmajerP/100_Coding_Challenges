5c577b21a789d14b499b9cf9079e57488a3c760e
DreamyChunk <107152151+DreamyChunk@users.noreply.github.com>
2022-12-07 21:43:16 +0100

    added day 8 of completed hallenges

--- day_8.py
+++ day_8.py
@@ -32,3 +32,34 @@
 # But Python has a built-in document function for every built-in functions.
 # Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
 # And add document for your own function
+
+# def doc(int):
+#     """Fuction that returns square number.
+
+#     parameter must be an integer
+#     """
+#     return (int**2)
+
+
+# help(range)
+# help(doc)
+
+
+# Question 25
+# Define a class, which have a class parameter and have a same instance parameter.
+# Hints:
+# Define an instance parameter, need add it in __init__ method.
+# You can init an object with construct parameter or set the value lat
+
+class User():
+    name = "User"
+
+    def __init__(self, name=None):
+        self.name = name
+
+
+john = User("John")
+print(User.name, john.name, )
+david = User()
+david.name = "David"
+print(User.name, david.name)
