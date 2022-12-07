7ee956fdc04e0b30fdfa2321c3a1c17ae0590d58
DreamyChunk <107152151+DreamyChunk@users.noreply.github.com>
2022-12-07 22:37:34 +0100

    added day 9 of challenges

---
+++ day_9.py
@@ -0,0 +1,38 @@
+# Question 26
+# Define a function which can compute the sum of two numbers.
+
+
+def my_sum(n1, n2): return n1+n2
+
+
+print(my_sum(2, 5))
+
+
+# Question 27
+# Define a function that can convert a integer into a string and print it in console.
+
+
+def convert(x): return str(x)
+
+
+print(convert(8))
+
+
+# Question 28
+# Define a function that can receive two integer numbers in string form
+# and compute their sum and then print it in console.
+
+
+def sum(x1, x2): return int(x1)+int(x2)
+
+
+print(sum("2", "8"))
+
+
+# Question 29
+# Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+def conc(w1, w2): return w1+w2
+
+
+print(conc("hello", " world"))
