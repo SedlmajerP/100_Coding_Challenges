248454d02a912435ef80772c99f936253b8bb8fc
DreamyChunk <107152151+DreamyChunk@users.noreply.github.com>
2022-12-08 21:23:27 +0100

    updated day 8

--- day_8.py
+++ day_8.py
@@ -5,26 +5,28 @@
 # New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
 
 
-# sentence = input().split()
-# lst = []
-# for w in sentence:
-#     x = (f"{w}:{sentence.count(w)}")
-#     lst.append(x)
-# sorted = sorted(lst)
-# for e in sorted:
-#     print(e)
+sentence = input().split()
+lst = []
+for w in sentence:
+    x = (f"{w}:{sentence.count(w)}")
+    lst.append(x)
+sorted = sorted(lst)
+for e in sorted:
+    print(e)
 
 
 # Question 23
 # Write a method which can calculate square value of number
 
-# class MyClass:
-#     def squared(self, num):
-#         print(num**2)
+
+class MyClass:
+    def squared(self, num):
+        print(num**2)
 
 
-# my_math = MyClass()
-# my_math.squared(7)
+my_math = MyClass()
+my_math.squared(7)
+
 
 # Question 24
 # Python has many built-in functions, and if you do not know how to use it,
@@ -33,23 +35,25 @@
 # Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
 # And add document for your own function
 
-# def doc(int):
-#     """Fuction that returns square number.
 
-#     parameter must be an integer
-#     """
-#     return (int**2)
+def doc(int):
+    """Fuction that returns square number.
+
+    parameter must be an integer
+    """
+    return (int**2)
 
 
-# help(range)
-# help(doc)
+help(range)
+help(doc)
 
 
 # Question 25
 # Define a class, which have a class parameter and have a same instance parameter.
 # Hints:
 # Define an instance parameter, need add it in __init__ method.
-# You can init an object with construct parameter or set the value lat
+# You can init an object with construct parameter or set the value later
+
 
 class User():
     name = "User"
